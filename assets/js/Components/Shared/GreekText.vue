77787c63b11673a46010106a1e93feb2bf58529e
Frederic Lamsens <frederic.lamsens@ugent.be>
2021-06-25 15:39:38 +0200

    add components

---
+++ assets/js/Components/Shared/GreekText.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="greek text" v-html="formatText(text)"></div>
+</template>
+
+<script>
+export default {
+    name: "GreekText",
+    props: {
+        text: {
+            type: String,
+        },
+    },
+    methods: {
+        formatText(text) {
+            const regexLineNumbers = /^([0-9]+)\./gm;
+            const replaceLineNumbers = '<span class="line-number">$1 </span>'
+            text = text.replace(regexLineNumbers, replaceLineNumbers);
+
+            return text
+        },
+    }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
