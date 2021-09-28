336b2045bd3c27873039d75e3a9da13bd4a2a3f4
Frederic Lamsens <frederic.lamsens@ugent.be>
2021-09-28 20:57:18 +0200

    phpdoc update

--- src/Service/ElasticSearch/Client.php
+++ src/Service/ElasticSearch/Client.php
@@ -10,12 +10,20 @@ class Client extends Elastica\Client
 {
     protected $indexPrefix;
 
+    /**
+     * @param $config
+     * @param null $indexPrefix
+     */
     public function __construct($config , $indexPrefix = null)
     {
         $this->indexPrefix = $indexPrefix;
         parent::__construct($config);
     }
 
+    /**
+     * @param string $name
+     * @return Index
+     */
     public function getIndex($name): Index
     {
         return parent::getIndex(($this->indexPrefix ? $this->indexPrefix .'_' : '').$name );
