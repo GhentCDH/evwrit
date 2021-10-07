84a52408c056f77ee0b71ff4b28cb4c48c2dcfef
Frederic Lamsens <frederic.lamsens@ugent.be>
2021-10-07 12:38:54 +0200

    add AnnotationDetailsFlat component

---
+++ assets/js/Components/Annotations/AnnotationDetailsFlat.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="annotation-details">
+        <span v-for="(detail, index) in details">
+            {{ index.replace(/([A-Z])/g, ' $1').replace(/^./, s => s.toUpperCase() ) }}: {{ detail }}
+        </span>
+    </div>
+</template>
+
+<script>
+
+import FormatValue from '../Sidebar/FormatValue'
+
+export default {
+    name: "AnnotationDetailsFlat",
+    components: {
+        FormatValue
+    },
+    props: {
+        annotation: {
+            type: Object,
+            required: true
+        },
+    },
+    computed: {
+        details() {
+            let ret = {};
+            ret['original'] = this.annotation.text_selection.text
+            //ret['selectionStart'] = this.annotation.text_selection.selection_start
+            //ret['selectionEnd'] = this.annotation.text_selection.selection_end
+
+            for (const prop in this.annotation.properties) {
+                if (this.annotation.properties.hasOwnProperty(prop)) {
+                    let value = this.annotation.properties[prop]
+                    if ( value && Array.isArray(value) && value.length ) {
+                        ret[prop] = value.map( i => i.id_name.split('_').slice(1).join('_') ).join(', ')
+                    } else if ( value && typeof value === 'object' && value.hasOwnProperty('id_name')) {
+                        ret[prop] = value.id_name.split('_').slice(1).join('_')
+                    }
+                }
+            }
+
+            delete ret.textSelection
+            console.log(ret)
+            return ret
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.annotation-details span {
+  display: inline-block;
+  margin: 0 1rem 3px 0;
+  color: #333;
+  background-color: #efefef;
+  padding: 2px 6px;
+  font-size: 90%;
+}
+</style>
