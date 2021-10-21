616e5b980ad617154e1b1e54ff9899d9eece6b6d
Frederic Lamsens <frederic.lamsens@ugent.be>
2021-10-21 14:01:51 +0200

    add checkbox-switch component

---
+++ assets/js/Components/Shared/CheckboxSwitch.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="checkbox checkbox-switch">
+        <label>
+            <input type="checkbox" :name="name" v-bind:checked="value" v-on:change="$emit('input', $event.target.checked)"/>
+            <span></span>
+            {{ label }}
+        </label>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CheckboxSwitch",
+    props: {
+        name: {
+            type: String,
+            required: false,
+            default: null
+        },
+        value: {
+            required: true,
+            type: Boolean
+        },
+        label: {
+            type: String
+        }
+    }
+}
+</script>
