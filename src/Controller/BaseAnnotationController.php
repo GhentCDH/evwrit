91f74dfefbab48cb1bf99b1cebc6de109df0dba1
Frederic Lamsens <frederic.lamsens@ugent.be>
2021-10-21 13:54:36 +0200

    code refactor split linguestic and lang/typo annotations

--- src/Controller/BaseAnnotationController.php
+++ src/Controller/BaseAnnotationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use App\Service\ElasticSearch\BaseAnnotationSearchService;
+use App\Service\ElasticSearch\AnnotationSearchService;
 
+use App\Service\ElasticSearch\LanguageTypographyAnnotationSearchService;
+use App\Service\ElasticSearch\LinguisticAnnotationSearchService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,21 +28,25 @@ class BaseAnnotationController extends BaseController
     }
 
     /**
-     * @Route("/annotation/search", name="annotation_search", methods={"GET"})
+     * @Route("/annotation/linguistic/search", name="annotation_linguistic_search", methods={"GET"})
      * @param Request $request
-     * @param BaseAnnotationSearchService $elasticService
+     * @param LinguisticAnnotationSearchService $elasticService
      * @return Response
      */
-    public function search(
+    public function linguisticSearch(
         Request $request,
-        BaseAnnotationSearchService $elasticService
+        LinguisticAnnotationSearchService $elasticService
     ) {
+        // update elastic service config
+
+
         return $this->render(
             $this->templateFolder. '/overview.html.twig',
             [
+                'title' => 'Linguistic annotations',
                 'urls' => json_encode([
                     // @codingStandardsIgnoreStart Generic.Files.LineLength
-                    'search_api' => $this->generateUrl('annotation_search_api'),
+                    'search_api' => $this->generateUrl('annotation_linguistic_search_api'),
                     'get_single' => $this->generateUrl('text_get_single', ['id' => 'text_id']),
                     // @codingStandardsIgnoreEnd
                 ]),
@@ -56,15 +62,72 @@ class BaseAnnotationController extends BaseController
     }
 
     /**
-     * @Route("/annotation/search_api", name="annotation_search_api", methods={"GET"})
+     * @Route("/annotation/linguistic/search_api", name="annotation_linguistic_search_api", methods={"GET"})
      * @param Request $request
-     * @param BaseAnnotationSearchService $elasticService
+     * @param LinguisticAnnotationSearchService $elasticService
      * @return JsonResponse
      */
-    public function searchAPI(
+    public function linguisticSearchAPI(
         Request $request,
-        BaseAnnotationSearchService $elasticService
+        LinguisticAnnotationSearchService $elasticService
     ) {
+        // update elastic service config
+
+        // search & aggregate
+        $result = $elasticService->searchAndAggregate(
+            $this->sanitize($request->query->all())
+        );
+
+        return new JsonResponse($result);
+    }
+
+    /**
+     * @Route("/annotation/languagetypography/search", name="annotation_languagetypography_search", methods={"GET"})
+     * @param Request $request
+     * @param LanguageTypographyAnnotationSearchService $elasticService
+     * @return Response
+     */
+    public function languageTypographySearch(
+        Request $request,
+        LanguageTypographyAnnotationSearchService $elasticService
+    ) {
+        // update elastic service config
+
+
+        return $this->render(
+            $this->templateFolder. '/overview.html.twig',
+            [
+                'title' => 'Language and Typography annotations',
+                'urls' => json_encode([
+                    // @codingStandardsIgnoreStart Generic.Files.LineLength
+                    'search_api' => $this->generateUrl('annotation_languagetypography_search_api'),
+                    'get_single' => $this->generateUrl('text_get_single', ['id' => 'text_id']),
+                    // @codingStandardsIgnoreEnd
+                ]),
+                'data' => json_encode(
+                    $elasticService->searchAndAggregate(
+                        $this->sanitize($request->query->all())
+                    )
+                ),
+                'identifiers' => json_encode([]),
+                'managements' => json_encode([])
+            ]
+        );
+    }
+
+    /**
+     * @Route("/annotation/languagetypography/search_api", name="annotation_languagetypography_search_api", methods={"GET"})
+     * @param Request $request
+     * @param LanguageTypographyAnnotationSearchService $elasticService
+     * @return JsonResponse
+     */
+    public function languageTypographySearchAPI(
+        Request $request,
+        LanguageTypographyAnnotationSearchService $elasticService
+    ) {
+        // update elastic service config
+
+        // search & aggregate
         $result = $elasticService->searchAndAggregate(
             $this->sanitize($request->query->all())
         );
