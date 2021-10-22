0131eb39edd03cf1447b7291e05cd597ec36f834
Frederic Lamsens <frederic.lamsens@ugent.be>
2021-10-22 12:53:47 +0200

    Persistent Cookie config for vue components

---
+++ assets/js/Components/Shared/PersistentConfig.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueCookies from 'vue-cookies'
+
+Vue.use(VueCookies)
+
+export default function(cookieName) {
+    return {
+        data() {
+            return {
+                configCookieName: cookieName,
+                config: {},
+            }
+        },
+        watch: {
+            config: {
+                handler: function (val, oldVal) { this.setCookie(this.configCookieName, val) },
+                deep: true
+            },
+        },
+        methods: {
+            setCookie(name, value) {
+                this.$cookies.set(name,value,'30d')
+            },
+            getCookie(name, defaultValue) {
+                try {
+                    let ret
+                    ret = this.$cookies.get(name)
+                    if (ret) {
+                        return ret
+                    }
+                } catch(error) {
+                    return defaultValue
+                }
+                return defaultValue;
+            },
+        },
+        mounted() {
+        },
+        created() {
+            this.config = this.defaultConfig;
+            if ( !this.$cookies.isKey(this.configCookieName) )
+                this.setCookie(this.configCookieName, this.config)
+            else {
+                this.config = this.getCookie(this.configCookieName,this.defaultConfig)
+            }
+        }
+    }
+}
