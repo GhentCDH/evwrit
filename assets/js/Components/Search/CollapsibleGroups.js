4d45dd017f190e16a53b7c4906181e34a08adcda
Frederic Lamsens <frederic.lamsens@ugent.be>
2022-01-17 11:52:31 +0100

    added support for PersistentConfig

--- assets/js/Components/Search/CollapsibleGroups.js
+++ assets/js/Components/Search/CollapsibleGroups.js
@@ -3,53 +3,36 @@ import VueCookies from 'vue-cookies'
 
 Vue.use(VueCookies)
 
-export default function(cookieName) {
+export default function() {
     return {
         data() {
             return {
-                groupIsOpen: [],
-                cookieName: cookieName
+                defaultConfig: {
+                    groupIsOpen: [],
+                }
             }
-        },
-        watch: {
-            groupIsOpen(newVal, oldVal) {
-                this.schema.groups.forEach(function (group, index) {
-                    group.styleClasses = (newVal[index] !== undefined && newVal[index]) ? 'collapsible' : 'collapsible collapsed'
-                })
-                this.setCookie(this.cookieName, this.groupIsOpen)
-            },
         },
         methods: {
             collapseGroup(e) {
                 const group = e.target.parentElement;
                 // get element index
                 let index = Array.from(group.parentNode.children).indexOf(group)
-                Vue.set(this.groupIsOpen, index, this.groupIsOpen[index] !== undefined ? !this.groupIsOpen[index] : true)
-            },
-            setCookie(name, value) {
-                this.$cookies.set(name,JSON.stringify(value),'30d')
-            },
-            getCookie(name, defaultValue) {
-                try {
-                    let ret
-                    ret = JSON.parse(this.$cookies.get(name))
-                    return ret
-                } catch(error) {
-                    return defaultValue
-                }
+                Vue.set(this.config.groupIsOpen, index, this.config.groupIsOpen[index] !== undefined ? !this.config.groupIsOpen[index] : true)
             },
         },
         mounted() {
             // make legends clickable
             const collapsableLegends = this.$el.querySelectorAll('.vue-form-generator .collapsible legend');
             collapsableLegends.forEach(legend => legend.onclick = this.collapseGroup);
+
+            // update group visibility on config change
+            this.$on('config-changed', function(config) {
+                if (config) {
+                    this.schema.groups.forEach(function (group, index) {
+                        group.styleClasses = group.styleClasses.replace(' collapsed','') + ((config.groupIsOpen[index] !== undefined && config.groupIsOpen[index]) ? '' : ' collapsed')
+                    })
+                }
+            })
         },
-        created() {
-            if ( !this.$cookies.isKey(this.cookieName) )
-                this.setCookie(this.cookieName, [])
-            else {
-                this.groupIsOpen = this.getCookie(this.cookieName,[])
-            }
-        }
     }
 }
