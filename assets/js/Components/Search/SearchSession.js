4c1e6cc13bdadb7945db96acf852792370c4d350
Frederic Lamsens <frederic.lamsens@ugent.be>
2022-01-27 10:30:46 +0100

    added SearchSession mixin

---
+++ assets/js/Components/Search/SearchSession.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import qs from "qs";
+import _merge from "lodash.merge";
+
+export default {
+    data() {
+        return {
+            defaultSearchSession: {
+                hash: null,
+                urls: {
+                },
+                count: 0,
+                params: {
+                }
+            },
+        }
+    },
+    methods: {
+        initSearchSession(data) {
+            let sessionData = this.defaultSearchSession;
+            sessionData = _merge(sessionData, data)
+            sessionData.hash = Date.now();
+            window.sessionStorage.setItem('search_session', JSON.stringify(sessionData));
+        },
+        updateSearchSession(data) {
+            let sessionData = this.getSearchSession();
+            sessionData = _merge(sessionData, data)
+            sessionData.hash = Date.now();
+            window.sessionStorage.setItem('search_session', JSON.stringify(sessionData));
+        },
+        getSearchSession(sessionHash) {
+            try {
+                let sessionData = JSON.parse(window.sessionStorage.getItem('search_session'));
+                if (sessionHash) {
+                    return (sessionData.hash == sessionHash ? sessionData : null)
+                } else {
+                    return sessionData;
+                }
+            } catch(e) {
+                return null;
+            }
+        },
+        getSearchSessionHash() {
+            return this.getSearchSession()?.hash ?? null
+        },
+    },
+}
