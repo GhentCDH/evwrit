7fea2e1bd4424bd92fd55ade4ad4b2318e9fbe7e
Frederic Lamsens <frederic.lamsens@ugent.be>
2022-01-31 10:32:04 +0100

    hide 'Unknown' values in attestations added tm_id in attestations

--- assets/js/apps/TextViewApp.vue
+++ assets/js/apps/TextViewApp.vue
@@ -113,7 +113,7 @@
 
                 <Widget title="Metadata" :is-open.sync="config.widgets.metadata.isOpen">
                     <LabelValue label="ID" :value="text.id"></LabelValue>
-                    <LabelValue label="Trismegistos ID" :value="text.tm_id" :url="urlTmId"></LabelValue>
+                    <LabelValue label="Trismegistos ID" :value="text.tm_id" :url="getTmTextUrl"></LabelValue>
 
                     <PropertyGroup>
                         <LabelValue label="Type" :value="text.text_type" type="id_name"></LabelValue>
@@ -168,14 +168,15 @@
                 <Widget title="Attestation"  :is-open.sync="config.widgets.attestation.isOpen" :count="text.ancient_person.length">
                     <template v-for="person in text.ancient_person">
                         <h3>{{ person.name }}</h3>
-                        <LabelValue label="Role" :value="person.role"  type="id_name"></LabelValue>
-                        <LabelValue label="Age" :value="person.age"  type="id_name"></LabelValue>
-                        <LabelValue label="Gender" :value="person.gender"  type="id_name"></LabelValue>
-                        <LabelValue label="Education" :value="person.education"  type="id_name"></LabelValue>
-                        <LabelValue label="Occupation" :value="person.occupation"  type="id_name"></LabelValue>
-                        <LabelValue label="Social Rank" :value="person.social_rank"  type="id_name"></LabelValue>
-                        <LabelValue label="Graph Type" :value="person.graph_type"  type="id_name"></LabelValue>
-                        <LabelValue label="Honorific Epithet" :value="person.honorific_epithet"  type="id_name"></LabelValue>
+                        <LabelValue label="Trismegistos ID" :value="person.tm_id" :url="getTmPersonUrl"></LabelValue>
+                        <LabelValue label="Role" :value="person.role"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
+                        <LabelValue label="Age" :value="person.age"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
+                        <LabelValue label="Gender" :value="person.gender"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
+                        <LabelValue label="Education" :value="person.education"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
+                        <LabelValue label="Occupation" :value="person.occupation"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
+                        <LabelValue label="Social Rank" :value="person.social_rank"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
+                        <LabelValue label="Graph Type" :value="person.graph_type"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
+                        <LabelValue label="Honorific Epithet" :value="person.honorific_epithet"  type="id_name" :ignore-value="['Unknown']"></LabelValue>
                     </template>
                 </Widget>
 
@@ -519,8 +520,11 @@ export default {
         getUrl(route) {
             return this.urls[route] ?? ''
         },
-        urlTmId(value) {
-            return 'https://www.trismegistos.org/text/' + value
+        getTmTextUrl(id) {
+            return 'https://www.trismegistos.org/text/' + id
+        },
+        getTmPersonUrl(id) {
+            return 'https://www.trismegistos.org/person/' + id
         },
         getTextUrl(id) {
             let url = this.urls['text_get_single'].replace('text_id', id);
