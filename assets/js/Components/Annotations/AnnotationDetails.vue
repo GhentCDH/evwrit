5625db3b97b675d49f9275d4403215f84a224791
Frederic Lamsens <frederic.lamsens@ugent.be>
2022-02-28 10:02:10 +0100

    hide properties not related to annotation type

--- assets/js/Components/Annotations/AnnotationDetails.vue
+++ assets/js/Components/Annotations/AnnotationDetails.vue
@@ -1,12 +1,8 @@
 <template>
     <div class="annotation__details">
-        <LabelValue label="Original" :value="annotation.text_selection.text" :inline="false" value-class="greek" class="greek mbottom-small"></LabelValue>
-        <LabelValue label="Annotation Type" :value="annotation.type" :inline="false"></LabelValue>
-        <LabelValue v-for="(value, label) in details" v-bind:key="label" :label="label" :value="value" :inline="false"></LabelValue>
-
-<!--            <span v-for="(detail, index) in details">-->
-<!--                {{ index.replace(/([A-Z])/g, ' $1').replace(/^./, s => s.toUpperCase() ) }}: {{ detail }}-->
-<!--            </span>-->
+        <LabelValue label="Original" :value="annotation.text_selection.text" :inline="false" value-class="greek" valueClass="greek" class="mbottom-small"></LabelValue>
+        <LabelValue label="Annotation Type" :value="annotation.type" :inline="false" class="mbottom-small"></LabelValue>
+        <LabelValue v-for="(value, label) in propertiesLabelValue" v-bind:key="label" :label="label" :value="value" :inline="true"></LabelValue>
     </div>
 </template>
 
@@ -26,24 +22,20 @@ export default {
         },
     },
     computed: {
-        details() {
+        properties() {
+            return Object.keys(this.annotation.properties).filter(k => k.startsWith(this.annotation.type + '_'))
+        },
+        propertiesLabelValue() {
             let ret = {};
-
-            for (let prop in this.annotation.properties) {
-                if (this.annotation.properties.hasOwnProperty(prop)) {
-                    // remove boolean property
-                    if (prop === 'has_handshift') {
-                        continue;
-                    }
-
-                    let value = this.annotation.properties[prop]
-                    let label = prop.split('_').slice(-1).join('') // strip type prefix
-                    label = label.replace(/([A-Z])/g, ' $1').replace(/^./, s => s.toUpperCase() )
-                    if ( value && Array.isArray(value) && value.length ) {
-                        ret[label] = value.map( i => i.id_name.split('_').slice(1).join('_') ).join(', ')
-                    } else if ( value && typeof value === 'object' && value.hasOwnProperty('id_name')) {
-                        ret[label] = value.id_name.split('_').slice(1).join('_')
-                    }
+            console.log(this.properties)
+            for (const prop of this.properties) {
+                let value = this.annotation.properties[prop]
+                let label = prop.split('_').slice(-1).join('') // strip type prefix
+                label = label.replace(/([A-Z])/g, ' $1').replace(/^./, s => s.toUpperCase() )
+                if ( value && Array.isArray(value) && value.length ) {
+                    ret[label] = value.map( i => i.id_name.split('_').slice(1).join('_') ).join(', ')
+                } else if ( value && typeof value === 'object' && value.hasOwnProperty('id_name')) {
+                    ret[label] = value.id_name.split('_').slice(1).join('_')
                 }
             }
 
