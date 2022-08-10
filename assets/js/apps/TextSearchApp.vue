79b957c8ed60eac22e11a188adf25d53da643cb4
Jacob <jacob.demuynck@ugent.be>
2022-08-10 11:08:17 +0000

    Change switch style

--- assets/js/apps/TextSearchApp.vue
+++ assets/js/apps/TextSearchApp.vue
@@ -224,11 +224,15 @@ export default {
                                 type: 'switch',
                                 label: 'Translated',
                                 model: 'has_translation',
+                                textOff: ' ',
+                                textOn: ' ',
                             },
                             {
                                 type: 'switch',
                                 label: 'Has image(s)',
                                 model: 'has_image',
+                                textOff: ' ',
+                                textOn: ' ',
                             },
                         ]
                     },
