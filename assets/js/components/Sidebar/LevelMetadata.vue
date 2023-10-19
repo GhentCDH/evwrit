8e9128297b5820c5f07ef659b36344eca2c7e619
Frederic Lamsens <frederic.lamsens@ugent.be>
2023-10-19 17:35:45 +0200

    updated level metadata (added greek/latin)

--- assets/js/components/Sidebar/LevelMetadata.vue
+++ assets/js/components/Sidebar/LevelMetadata.vue
@@ -13,16 +13,23 @@
 
         <LabelValue label="Production stage" :value="level.production_stage" :inline="true" type="id_name"></LabelValue>
 
-        <LabelValue label="Agentive role">
-            <div v-for="role in level.agentive_role" class="agentive-role">
+        <LabelValue label="Agentive role" v-if="level?.agentive_role?.length">
+            <div v-for="role in level?.agentive_role" class="agentive-role">
                 <FormatValue type="id_name" :value="role.generic_agentive_role"></FormatValue>,
                 <FormatValue type="id_name" :value="role.agentive_role"></FormatValue>
             </div>
         </LabelValue>
-        <LabelValue label="Communicative goal">
-            <div v-for="goal in level.communicative_goal" class="communicative-goal">
+        <LabelValue label="Communicative goal" v-if="level?.communicative_goal?.length">
+            <div v-for="goal in level?.communicative_goal" class="communicative-goal">
                 <FormatValue type="id_name" :value="goal.communicative_goal_type"></FormatValue>,
                 <FormatValue type="id_name" :value="goal.communicative_goal_subtype"></FormatValue>
+            </div>
+        </LabelValue>
+
+        <LabelValue label="Genre" v-if="level?.greek_latin?.length">
+            <div v-for="greek_latin in level?.greek_latin" class="span-list span-list--comma-separated">
+                <FormatValue type="string" :value="greek_latin.label"></FormatValue>
+                (<FormatValue type="string" :value="greek_latin.english"></FormatValue>)
             </div>
         </LabelValue>
 
