16c766b14f6831aff9b886a76af629d8cbde3c29
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-11-04 11:55:01 +0100

    chore: wait for elasticsearch to be up in startup script

--- app/startup-script.sh
+++ app/startup-script.sh
@@ -12,13 +12,35 @@ done
 # install backend dependencies
 echo "Installing backend dependencies"
 composer install
-echo "Backend dependencies installed"
 echo "Dumping environment variables to .env.local.php"
 composer dump-env dev
 
 # clear cache
 echo "Clearing cache..."
 php bin/console cache:clear
+
+# wait for Elasticsearch to be up
+until curl -s "elasticsearch:9200" > /dev/null; do
+    echo "Waiting for Elasticsearch to be up..."
+    sleep 5
+done
+
+# check elasticesearch indices
+curl -f -s -I "elasticsearch:9200/${ELASTICSEARCH_INDEX_PREFIX}_texts" > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Text index already exists"
+else
+    echo "Creating text index (100 records max) ..."
+    php bin/console app:elasticsearch:index text 100
+fi
+
+curl -f -s -I "elasticsearch:9200/${ELASTICSEARCH_INDEX_PREFIX}_level" > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Level index already exists"
+else
+    echo "Creating level index (100 records max) ..."
+    php bin/console app:elasticsearch:index level 100
+fi
 
 # start the symfony server
 echo "Starting server..."
