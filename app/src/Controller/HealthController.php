3f664ced4b677f06597b9903e69db85647de282f
Bo Vandersteene <bo.vandersteene@firepeak.tech>
2024-12-10 11:32:54 +0100

    fix: add health endpoint

--- app/src/Controller/HealthController.php
+++ app/src/Controller/HealthController.php
@@ -2,20 +2,9 @@
 
 namespace App\Controller;
 
-use App\Helper\StreamedCsvResponse;
-use App\Repository\TextRepository;
-use App\Resource\ElasticTextAnnotationsResource;
-use App\Resource\TextSearchFlagsResource;
-use Exception;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route;
-use Throwable;
 
 class HealthController extends BaseController
 {
@@ -23,7 +12,7 @@ class HealthController extends BaseController
     /**
      * @Route("/health", name="health", methods={"GET"})
      * @param Request $request
-     * @return RedirectResponse
+     * @return JsonResponse
      */
     public function health(Request $request)
     {
