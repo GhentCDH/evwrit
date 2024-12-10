45edfa5955d37d78ced3dbd60c81d3d0ff44d7a7
Bo Vandersteene <bo.vandersteene@firepeak.tech>
2024-12-10 11:52:26 +0100

    fix: add health endpoint

--- app/src/Controller/HealthController.php
+++ app/src/Controller/HealthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 
-class HealthController extends BaseController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HealthController extends AbstractController
 {
 
     /**
