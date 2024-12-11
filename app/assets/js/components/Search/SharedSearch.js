4f0ab41067fdf7b49400060655e86ce629607957
jchetti <jahid.chetti@ugent.be>
2024-12-11 16:03:07 +0100

    removed unnecessary code

--- app/assets/js/components/Search/SharedSearch.js
+++ app/assets/js/components/Search/SharedSearch.js
@@ -47,15 +47,9 @@ export default {
             return params;
         },
         handleLinkClick(event){
+            event.preventDefault();
             if (event.button === 0 || event.button === 1){
-                event.preventDefault();
                 this.$cookies.set(`${event.target.getAttribute("href")}_prev_url`, window.location.href, '1d');
-                if (event.button === 0){
-                    window.open(event.target.getAttribute("href"));
-                } else {
-                    window.open(event.target.getAttribute("href"), '_blank');
-                }
-
             }
         },
     },
