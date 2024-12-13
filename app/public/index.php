4fbc739278bd94a527026f4b3761dbe9af8acf92
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-12-13 11:21:42 +0100

    chore: remove Dotenv dependency (not using symfony .env files from now on)

--- app/public/index.php
+++ app/public/index.php
@@ -1,17 +1,16 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
+//use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
+//(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
-
     Debug::enable();
 }
 
