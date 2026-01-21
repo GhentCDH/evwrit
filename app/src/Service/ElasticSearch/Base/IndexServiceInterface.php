e2892ea3e11804f212c6c8f3dad5111a7fd4dbf4
Frederic Lamsens <frederic.lamsens@ugent.be>
2026-01-21 18:57:52 +0100

    feat: add update/updateAll methods to Elastic indexer

--- app/src/Service/ElasticSearch/Base/IndexServiceInterface.php
+++ app/src/Service/ElasticSearch/Base/IndexServiceInterface.php
@@ -2,6 +2,17 @@
 
 namespace App\Service\ElasticSearch\Base;
 
+use App\Resource\ResourceInterface;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
 interface IndexServiceInterface
 {
+    public function setup(): void;
+
+    public function get(string $id): array|string;
+    public function delete(string $id): void;
+    public function deleteMultiple(array $ids): void;
+    public function add(ResourceInterface $resource): void;
+    public function update(ResourceInterface $resource): void;
+    public function addMultiple(ResourceCollection $resources): void;
 }
