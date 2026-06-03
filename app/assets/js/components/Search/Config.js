ee327383494953d207fcf6c881a39d1f95a973e2
Frederic Lamsens <frederic.lamsens@ugent.be>
2026-06-03 18:40:17 +0200

    refactor: calculated text stats are now expert only

--- app/assets/js/components/Search/Config.js
+++ app/assets/js/components/Search/Config.js
@@ -240,7 +240,7 @@ export default {
             }
         },
         materialityFields(expertOnly = false) {
-            return {
+            return filters = {
                 expertOnly: expertOnly,
                 visible: this.fieldVisible,
                 collapsible: true,
@@ -300,20 +300,55 @@ export default {
                     this.createRangeSlider('margin_top','Margin Top',0,350,0.01, 2),
                     this.createRangeSlider('margin_bottom','Margin Bottom',0,1050,0.01, 2),
 
-                    //whitespace
-                    this.createRangeSlider('whitespace_area', 'Whitespace Area', 0, 10000, 1),
-                    this.createRangeSlider('whitespace_percentage', 'Whitespace %', 0, 100, 1),
-                    this.createRangeSlider('total_area', 'Total Document Area', 0, 10000, 1),
-                    this.createRangeSlider('used_area', 'Used Area', 0, 10000, 1),
-                    this.createRangeSlider('central_width', 'Central Width', 0, 300, 0.01, 2),
-                    this.createRangeSlider('central_height', 'Central Height', 0, 300, 0.01, 2),
+                    //whitespace caulculated fields
+                    // expert only fields
+                    { ...this.createRangeSlider('whitespace_area', 'Whitespace Area', 0, 10000, 1),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('whitespace_percentage', 'Whitespace %', 0, 100, 1),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('total_area', 'Total Document Area', 0, 10000, 1),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('used_area', 'Used Area', 0, 10000, 1),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('central_width', 'Central Width', 0, 300, 0.01, 2),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('central_height', 'Central Height', 0, 300, 0.01, 2),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
 
                     //other calculated fields
-                    this.createRangeSlider('width_height_ratio', 'Width to Height ratio', 0, 5, 0.01, 2),
-                    this.createRangeSlider('lineheight_interlinearspace_ratio', "Line Height to Interlinear Space ratio", 0, 5, 0.01, 2),
-                    this.createRangeSlider('average_line_space', 'Average Line Space', 0, 100, 0.01, 2),
-                    this.createRangeSlider('average_letter_space', 'Average Letter Space', 0, 10, 0.01, 2),
-                    this.createRangeSlider('average_words_per_line', 'Average Words per Line', 0, 100, 0.01, 2),
+                    // expert only fields
+                    { ...this.createRangeSlider('width_height_ratio', 'Width to Height ratio', 0, 5, 0.01, 2),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('lineheight_interlinearspace_ratio', "Line Height to Interlinear Space ratio", 0, 5, 0.01, 2),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('average_line_space', 'Average Line Space', 0, 100, 0.01, 2),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('average_letter_space', 'Average Letter Space', 0, 10, 0.01, 2),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
+                    { ...this.createRangeSlider('average_words_per_line', 'Average Words per Line', 0, 100, 0.01, 2),
+                        expertOnly: true,
+                        visible: this.fieldVisible
+                    },
 
                     // global typography fields
                     this.createMultiSelect('Drawings', {
